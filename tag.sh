78067285a7f929c4d4a9a04c8bb6c793f377c4ca
anxiwuyanzu <anxiwuyanzu@anxiwuyanzudeMac-mini.local>
2026-03-31 23:39:03 +0800

    chore: remove legacy tag script (standardized tagging via git tags)

--- tag.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-Release=$(git rev-list v4.0.0.. --count)
-git tag -a "v4.0.${Release}" -m "${Release}"
-git remote set-url origin 'git@github.com:anxiwuyanzu/openscraper-framework.git'
-
-git push origin --tag
